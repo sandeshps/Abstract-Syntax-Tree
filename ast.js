1488b0cd76835f203b16ddcd8e9280081000c1de
sandeshps <sandeshps1989@yahoo.com>
2013-09-22 19:15:39 +0530

    Added traversal code

--- ast.js
+++ ast.js
@@ -1,5 +1,41 @@
-var esprima = require("esprima");
+/*
+   This program will take
+
+
+ */
+
+
+var esp = require('esprima');
+
+function traverse(node, func) {
+    func(node);
+    for (var key in node) {
+        if (node.hasOwnProperty(key)) {
+            var child = node[key];
+            if (typeof child === 'object' && child !== null) {
+
+                if (Array.isArray(child)) {
+                    child.forEach(function(node) {
+                        traverse(node, func);
+                    });
+                } else {
+                    traverse(child, func);
+                }
+            }
+        }
+    }
+}
 
 function analyzeCode(code) {
-    // TODO code here
+    var ast = esp.parse(code); // Parse the code
+    traverse(ast,function(node){
+        console.log(node.type);
+    })
 }
+
+var codeFragment = "function test() { var a=10; b=20; }"     ;
+analyzeCode(codeFragment);
+
+
+
+
