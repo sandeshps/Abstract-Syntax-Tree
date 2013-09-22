bac3fc65c39f970249b926037d8e3aee420befd0
sandeshps <sandeshps1989@yahoo.com>
2013-09-22 20:39:55 +0530

    Tested the code with function snippet. Working

--- ast.js
+++ ast.js
@@ -1,11 +1,12 @@
 /*
    Program using 'esprima' for checking the non-local variables inside a function. The function is passed as input to
-   this code
+   this code.
  */
 
 
 var esp = require('esprima');
 
+// Code for visiting each and every node of a function passed
 function traverse(node, func) {
     func(node);
     for (var key in node) {
@@ -25,15 +26,23 @@ function traverse(node, func) {
     }
 }
 
+// Code that checks the function
 function analyzeCode(code) {
+    var countOfGlobalVariables = 0;
     var ast = esp.parse(code); // Parse the code
     traverse(ast,function(node){
-        console.log(node.type);
-    })
+        //console.log(node.type);
+        // If the node type is ExpressionStatement, then it is a variable of gloabal scope(non-local)
+        if(node.type == 'ExpressionStatement'){
+            ++countOfGlobalVariables;
+        }
+     })
+    console.log("Number of global (non-local) variables is "+ countOfGlobalVariables); // Display the number of global variables
 }
 
-var codeFragment = "function test() { var a=10; b=20; }"     ;
-analyzeCode(codeFragment);
+
+var codeFragment = "function test() {  a=10;b=20;c=30;var f=20; }"; // string variable that holds the function to be passed
+analyzeCode(codeFragment); // Pass the input function
 
 
 
